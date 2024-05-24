96be87355a9442c907fb0215106eca5daf5d8280
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-05-24 17:41:53 -0500

    Added TailwindCSS

--- cc-portfolio/src/App.jsx
+++ cc-portfolio/src/App.jsx
@@ -1,25 +1,18 @@
-import { useState } from 'react'
+//style sheet
 import './App.css'
+//Component Sections
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
