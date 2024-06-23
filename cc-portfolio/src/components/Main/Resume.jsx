09be0c37e8f08a358ccf6a9458afffbff0f56023
codydecoder <142354876+codydecoder@users.noreply.github.com>
2024-06-22 19:42:15 -0500

    Added form component

--- cc-portfolio/src/components/Main/Resume.jsx
+++ cc-portfolio/src/components/Main/Resume.jsx
@@ -5,8 +5,9 @@ import resume from './CCResume.jpg'
 
 export default function Resume() {
     return (
-        <div className='border-4 border-black w-full'>
-            <img src={resume} alt="Cody Cadena Resume" className=''/>
+        <div className='w-full'>
+            <img src={resume} alt="Cody Cadena Resume" className='resume'/>
+            
         </div>
     )
 }
